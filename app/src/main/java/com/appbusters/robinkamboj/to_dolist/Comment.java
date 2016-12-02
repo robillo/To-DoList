4c6c681a2252416e849c2e36a7f242c237298d43
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-03 00:31:53 +0530

    added Comment class

--- app/src/main/java/com/appbusters/robinkamboj/to_dolist/Comment.java
+++ app/src/main/java/com/appbusters/robinkamboj/to_dolist/Comment.java
@@ -7,4 +7,27 @@ package com.appbusters.robinkamboj.to_dolist;
 public class Comment {
     private  long id;
     private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    //Will be used by the RecyclerViewAdapter in the RecyclerView
+
+    @Override
+    public String toString() {
+        return comment;
+    }
 }
