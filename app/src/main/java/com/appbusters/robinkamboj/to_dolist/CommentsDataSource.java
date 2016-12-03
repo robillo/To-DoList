0511034c2ed5a7d137c8337ff56313110f6ac531
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-03 18:01:29 +0530

    Creating DA0 class

--- app/src/main/java/com/appbusters/robinkamboj/to_dolist/CommentsDataSource.java
+++ app/src/main/java/com/appbusters/robinkamboj/to_dolist/CommentsDataSource.java
@@ -1,8 +1,14 @@
 package com.appbusters.robinkamboj.to_dolist;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by robinkamboj on 03/12/16.
@@ -27,5 +33,40 @@ public class CommentsDataSource {
 
     public void close(){
         dbHelper.close();
+    }
+
+    public Comment createComment(String comment){
+        ContentValues values= new ContentValues();
+        values.put(MyDbHelper.COLUMN_COMMENT,comment);
+        long insertId= database.insert(MyDbHelper.TABLE_COMMENTS,null,values);
+        Cursor cursor= database.query(MyDbHelper.TABLE_COMMENTS,allColumns,MyDbHelper.COLUMN_ID + "=" + insertId,null,null,null,null);
+        cursor.moveToFirst();
+        Comment newComment= cursorToComment(cursor);
+        cursor.close();
+        return newComment;
+    }
+
+    public void deleteComment(Comment comment){
+        long id= comment.getId();
+        System.out.println("Comment deleted with id: " + id);
+        database.delete(MyDbHelper.TABLE_COMMENTS,MyDbHelper.COLUMN_ID + "=" + id, null);
+    }
+
+    public List<Comment> getAllComments(){
+        List<Comment> comments= new ArrayList<Comment>();
+        Cursor cursor= database.query(MyDbHelper.TABLE_COMMENTS, allColumns, null, null, null, null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            Comment comment= cursorToComment(cursor);
+        }
+        cursor.close();
+        return comments;
+    }
+
+    private Comment cursorToComment(Cursor cursor){
+        Comment comment= new Comment();
+        comment.setId(cursor.getLong(0));
+        comment.setComment(cursor.getString(1));
+        return  comment;
     }
 }
